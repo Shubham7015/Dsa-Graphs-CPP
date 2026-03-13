60e3ca17b54cf09b18447764d66bb37feee36ade
Shubham Rohilla <srohilla099@gmail.com>
2026-03-13 20:20:25 +0530

    added comments and make a keynotes file to know when a graph is a bipartite graph or not

--- Problems Day 6/isGraphBipartite_Leetcode785BFS.cpp
+++ Problems Day 6/isGraphBipartite_Leetcode785BFS.cpp
@@ -1,53 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std ;
 
+// Class to solve the bipartite graph problem using BFS
 class Solution {
 private:
+    // Helper function to check if the component containing 'node' is bipartite
     bool check(int node, vector<int>& color, vector<vector<int>>& graph) {
         queue<int> q;
         q.push(node);
-        color[node] = 0;
+        color[node] = 0; // Start coloring the node with color 0
 
         while (!q.empty()) {
             int node = q.front();
             q.pop();
 
+            // Traverse all adjacent nodes
             for (int it : graph[node]) {
 
-                // if not yet coloured colour to opposite of node
-
+                // If the adjacent node is not colored, color it with opposite color
                 if (color[it] == -1) {
                     q.push(it);
                     color[it] = !color[node];
                 }
-                // someone coloured you before on some other path
+                // If the adjacent node has the same color, graph is not bipartite
                 else if (color[it] == color[node]) {
                     return false;
                 }
             }
         }
-        return true;
+        return true; // Component is bipartite
     }
 
 public:
+    // Main function to check if the entire graph is bipartite
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
 
-        vector<int> color(n, -1); // for colouring the nodes
+        vector<int> color(n, -1); // Initialize all nodes as uncolored
 
+        // Check each component of the graph
         for (int i = 0; i < n; i++) {
-            if (color[i] == -1) {
+            if (color[i] == -1) { // If node is not colored
                 if (check(i, color, graph) == false)
-                    return false;
+                    return false; // If any component is not bipartite
             }
         }
-        return true;
+        return true; // All components are bipartite
     }
 };
 
 int main() {
+    // Example graph: 4 nodes, edges between 0-1, 0-3, 1-2, 2-3
     vector<vector<int>> graph = {{1,3},{0,2},{1,3},{0,2}} ;
     Solution sol ;
 
-    cout<< sol.isBipartite(graph) ;
+    cout<< sol.isBipartite(graph) ; // Output: 1 (true)
 }
--- Problems Day 6/isGraphBipartite_Leetcode785DFS.cpp
+++ Problems Day 6/isGraphBipartite_Leetcode785DFS.cpp
@@ -4,32 +4,34 @@ using namespace std ;
 class Solution {
     private:
     bool check(int node , int col, vector<int>&color , vector<vector<int>>&adj) {
-        color[node] = col ; 
+        color[node] = !col ; // color it with oppose color from adjacent node 
         
         for(int it : adj[node]) {
-            if(color[it] == -1) {
-                if(check(it,!col,color,adj) == false) return false ;
+            if(color[it] == -1) { // if not coloured till now coloured it 
+                if(check(it,col,color,adj) == false) return false ; // if it is false return false 
             }
-            else if(color[it] == col) return false ;
+            else if(color[it] == col) return false ;// if adjacent node color and current node color is same means not a bipartite graph so return false here also 
+        
         }
-        return true ;
+        return true ; // if satisfies all the condition return true ;
     }
   public:
     bool isBipartite(int n, vector<vector<int>> &edges) {
-         vector<vector<int>> adj(n) ;
+         vector<vector<int>> adj(n) ; 
          
-         for(auto e  :edges){
+         for(auto e  :edges){// make adjacency list 
              adj[e[0]].push_back(e[1]) ;
              adj[e[1]].push_back(e[0]) ;
          }
-        vector<int>color(n,-1) ;
+
+        vector<int>color(n,-1) ;// for coloring the nodes or vertices or checking if it is coloured or not 
         
-        for(int i = 0; i < n ; i++) {
-            if(color[i] == -1){
-                if(check(i,0,color,adj) == false) return false ;
+        for(int i = 0; i < n ; i++) { // for checking all the components either connected or not connected 
+            if(color[i] == -1){// if not coloured till now 
+                if(check(i,0,color,adj) == false) return false ; // if it return false not a bipartite graph
             }
         }
-        return true ;
+        return true ; // return true bipartite graph 
         
     }
 };
